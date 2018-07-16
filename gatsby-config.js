69607e250af1fe3376c31a56396b30ea16925fb0
Bryan Grill <grillb@gmail.com>
2018-07-16 12:04:58 -0400

    fix deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,6 @@ module.exports = {
     title: 'WAB & React',
     date: 'March 8, 2018',
   },
+  pathPrefix: '/esri-dev-summit-presentation-2018',
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-offline'],
 };
